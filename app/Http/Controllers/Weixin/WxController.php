fd2dfefb282998e53996e16633f5b35c2a489ea9
ftyss <1251898775@qq.com>
2019-12-09 12:05:36 +0000

    改错

--- app/Http/Controllers/Weixin/WxController.php
+++ app/Http/Controllers/Weixin/WxController.php
@@ -9,18 +9,26 @@ class WxController extends Controller
 {
     protected $access_token;
 
+
     public function __construct()
     {
-        //获取access_token
-        $this->access_token=$this->getAccessToken();
+        // 获取 access_token
+        $this->access_token = $this->getAccessToken();
     }
+
+
+
 
     protected function getAccessToken()
     {
-        $url='https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.env('WX_APPID').'&secret='.env('WX_APPSECRET');
-        $data_json=file_get_contents($url);
-        $arr=json_decode($data_json,true);
+        $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.env('WX_APPID').'&secret='.env('WX_APPSWCRET');
+        $data_json = file_get_contents($url);
+        $arr = json_decode($data_json,true);
         return $arr['access_token'];
+
+
+
+
     }
 
     /**
@@ -49,41 +57,76 @@ class WxController extends Controller
 
     /**
      * 接收微信推送事件
-     */
-    public function receiv()
-    {
-        $log_file="wx.log";         //默认写在public目录下
-        //将接收到的数据添加到日志中
-        $xml_str=file_get_contents("php://input");
-        $data=date('Y-m-d H:i:s') . $xml_str;
-        file_put_contents($log_file,$data,FILE_APPEND);     //FILE_APPEND追加写
-        $xml_arr=simplexml_load_string($xml_str);
+    */ 
+    // public function receiv()
+    // {
+    //     $log_file="wx.log";         //默认写在public目录下
+    //     //将接收到的数据添加到日志中
+    //     $xml_str=file_get_contents("php://input");
+    //     $data=date('Y-m-d H:i:s') . $xml_str;
+    //     file_put_contents($log_file,$data,FILE_APPEND);     //FILE_APPEND追加写
+    //     $xml_arr=simplexml_load_string($xml_str);
 
-        //处理xml数据
-        $xml_obj=simplexml_load_string($xml_str);
+    //     //处理xml数据
+    //     $xml_obj=simplexml_load_string($xml_str);
 
-        $event=$xml_obj->Event;         //获取事件类型
-        if($event=='subscribe'){
-            $openid=$xml_obj->FromUserName;     //获取用户的openid
-            //获取用户基本信息
-        $url='https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$this->access_token.'&openid='.$openid.'&lang=zh_CN';
-        $user_info=file_get_contents($url);
-        file_put_contents('wx_user.log',$user_info,FILE_APPEND);
-        }
+    //     $event=$xml_obj->Event;         //获取事件类型
+    //     if($event=='subscribe'){
+    //         $openid=$xml_obj->FromUserName;     //获取用户的openid
+    //         //获取用户基本信息
+    //     $url='https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$this->access_token.'&openid='.$openid.'&lang=zh_CN';
+    //     $user_info=file_get_contents($url);
+    //     file_put_contents('wx_user.log',$user_info,FILE_APPEND);
+    //     }
 
         
+    // }
+
+    public function receiv()
+    {
+        $log_file = "wx.log";
+        //将接收的数据记录到日志文件
+        $xml_str = file_get_contents("php://input");
+        $data =date('Y-m-d H:i:s')  . $xml_str;
+        file_put_contents($log_file,$data,FILE_APPEND);   //追加写
+    
+        $xml_arr = simplexml_load_string($xml_str);
+
+
+
+
+        // 处理xml数据
+        $xml_obj = simplexml_load_string($xml_str);
+
+
+        $event = $xml_obj->Event;   //获取事件类型
+        if($event=='subscribe'){
+            $openid = $xml_obj->FromUserName;   // 获取用户的openid
+            //获取用户信息
+            $url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$this->access_token.'&openid='.$openid.'&lang=zh_CN';
+            $user_info = file_get_contents($url);   //
+            file_put_contents('wx_user.log',$user_info,FILE_APPEND);
     }
 
 
     /**
      * 获取用户基本信息
      */
+    // public function getUserInfo($access_token,$openid)
+    // {
+    //     $url="https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN";
+    //     //发送网络请求
+    //     $json_obj=file_get_contents($url);
+    //     $log_file="wx_user.log";
+    //     file_put_contents($log_file,$json_obj,FILE_APPEND);
+    // }
+
     public function getUserInfo($access_token,$openid)
     {
-        $url="https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN";
-        //发送网络请求
-        $json_obj=file_get_contents($url);
-        $log_file="wx_user.log";
-        file_put_contents($log_file,$json_obj,FILE_APPEND);
+        $url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
+        // 发送网络请求
+        $json_str = file_get_contents($url);
+        $log_file = 'wx_user.log';
+        file_put_contents($log_file,$json_str,FILE_APPEND);
     }
 }
