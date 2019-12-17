e8aa1717c50b83ea89e88640364a20a2588b1dbb
ftyss <1251898775@qq.com>
2019-12-17 10:21:54 +0000

    调用天气接口

--- app/Http/Controllers/Weixin/WxController.php
+++ app/Http/Controllers/Weixin/WxController.php
@@ -128,11 +128,21 @@ class WxController extends Controller
             }
         }elseif($event=='CLICK'){           //点击事件
             if($xml_obj->EventKey=='tianqi'){
+                $weather_api='https://free-api.heweather.net/s6/weather/now?location=beijing&key=0eced2803331478083c559f863fe4924';
+                $weather_info=file_get_contents($weather_api);
+                $weather_info_arr=json_decode($weather_info,true);
+                //echo '<pre>';print_r($weather_info_arr);echo '</pre>';die;
+                $cond_txt=$weather_info_arr['HeWeather6'][0]['now']['cond_txt'];
+                $tmp=$weather_info_arr['HeWeather6'][0]['now']['tmp'];
+                $wind_dir=$weather_info_arr['HeWeather6'][0]['now']['wind_dir'];
+
+                $msg=$cond_txt .'温度： '.$tmp . '风向： '. $wind_dir;
+
                 $response_xml='<xml><ToUserName><![CDATA['.$openid.']]></ToUserName>
                 <FromUserName><![CDATA['.$xml_obj->ToUserName.']]></FromUserName>
                 <CreateTime>'.time().'</CreateTime>
                 <MsgType><![CDATA[text]]></MsgType>
-                <Content><![CDATA['.date('Y-m-d H:i:s').'  漫天飞雪'.']]></Content>
+                <Content><![CDATA['.date('Y-m-d H:i:s').$msg.']]></Content>
                 </xml>';
                 echo $response_xml;
             }
@@ -298,7 +308,7 @@ class WxController extends Controller
                 [
                     'type' => 'click',
                     'name' => '获取天气',
-                    'key' => 'tianqi'
+                    'key' => 'weather'
                 ],
                 [
                     'name'=>'菜单',
