59ccfe9f1955b7aaab50a7a79d0b0fae066fbca5
ftyss <1251898775@qq.com>
2019-12-18 06:05:43 +0000

    voteBug

--- app/Http/Controllers/Vote/VoteController.php
+++ app/Http/Controllers/Vote/VoteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Vote;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class VoteController extends Controller
 {
--- app/Http/Controllers/Weixin/WxController.php
+++ app/Http/Controllers/Weixin/WxController.php
@@ -324,7 +324,7 @@ class WxController extends Controller
                         [
                             'type' => 'view',
                             'name' => '投票',
-                            'url' => 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$redirect_uri.'&response_type=code&scope=snsapi_userinfo&state=asd123#wechat_redirect'
+                            'url' => 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.env('WX_APPID').'&redirect_uri='.$redirect_uri.'&response_type=code&scope=snsapi_userinfo&state=asd123#wechat_redirect'
                         ],
                        
                     ]
