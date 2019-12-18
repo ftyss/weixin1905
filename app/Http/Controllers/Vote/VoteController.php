bf276e668555ed0fdd17c89fb898eb73b35b4da1
ftyss <1251898775@qq.com>
2019-12-18 07:30:13 +0000

    vote获取用户基本信息

--- app/Http/Controllers/Vote/VoteController.php
+++ app/Http/Controllers/Vote/VoteController.php
@@ -13,7 +13,10 @@ class VoteController extends Controller
         echo '<pre>';print_r($_GET);echo '</pre>';
         $code=$_GET['code'];
 
-        $this->getAccessToken($code);   //获取access_token
+        $data = $this->getAccessToken($code);   //获取access_token
+
+        //获取用户信息
+        $user_info=$this->getUserInfo($data['access_token'],$data['openid']);
     }
 
     /**
@@ -23,7 +26,17 @@ class VoteController extends Controller
     {
         $url='https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('WX_APPID').'&secret='.env('WX_APPSECRET').'&code='.$code.'&grant_type=authorization_code';
         $json_data=file_get_contents($url);
-        $data=json_decode($json_data,true);
-        echo '<pre>';print_r($data);echo '</pre>';
+        return json_decode($json_data,true);
     }
+
+    /**
+     * 获取用户基本信息
+     */
+    protected function getUserInfo($access_token,$openid)
+    {
+        $url='https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
+        $json_data=file_get_contents($url);
+        $usr_info=json_decode($json_data,true);
+        echo '<pre>';print_r($usr_info);echo '</pre>';
+    }    
 }
