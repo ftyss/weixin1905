42f8c25fb6e168ad234387fa26d8b6e258f0c2b7
ftyss <1251898775@qq.com>
2019-12-18 06:32:32 +0000

    通过code获取access_token

--- app/Http/Controllers/Vote/VoteController.php
+++ app/Http/Controllers/Vote/VoteController.php
@@ -10,6 +10,20 @@ class VoteController extends Controller
 {
     public function index()
     {
-        echo "123";
+        echo '<pre>';print_r($_GET);echo '</pre>';
+        $code=$_GET['code'];
+
+        $this->getAccessToken($code);   //获取access_token
+    }
+
+    /**
+     * 根据code获取access_token
+     */
+    protected function getAccessToken($code)
+    {
+        $url='https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('WX_APPID').'&secret='.env('WX_APPSECRET').'&code='.$code.'&grant_type=authorization_code';
+        $json_data=file_get_contents($url);
+        $data=json_decode($json_data,true);
+        echo '<pre>';print_r($data);echo '</pre>';
     }
 }
