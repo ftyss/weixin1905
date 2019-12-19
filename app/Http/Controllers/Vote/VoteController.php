045cf777adbd9e041d17e36a34d5d92db7992238
ftyss <1251898775@qq.com>
2019-12-19 06:58:24 +0000

    a

--- app/Http/Controllers/Vote/VoteController.php
+++ app/Http/Controllers/Vote/VoteController.php
@@ -42,7 +42,7 @@ class VoteController extends Controller
             $u=Redis::hgetAll($u_k);
             //$u=Redis::hMget($u_k,['openid','nickname','sex','headimgurl']);
             //echo '<pre>';print_r($u);echo '</pre>';
-            echo '<img src="'.$u['headimgurl'].'">'  ; 
+            echo '<img src="'.$u['headimgurl'].'">   '; 
         }
         
     }
