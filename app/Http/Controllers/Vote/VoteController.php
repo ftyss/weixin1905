1b4b5d95492a6bde1763e4ad38f19e10b3704524
ftyss <1251898775@qq.com>
2019-12-19 06:21:56 +0000

    获取投票用户头像

--- app/Http/Controllers/Vote/VoteController.php
+++ app/Http/Controllers/Vote/VoteController.php
@@ -36,11 +36,13 @@ class VoteController extends Controller
         $members=Redis::zRange($key,0,-1,true);         //获取所有投票用户的openid
         //echo '<pre>';print_r($members);echo '</pre>';
         foreach($members as $k=>$v){
-            echo "用户：".$k .'投票时间：'. date('Y-m-d H:i:s',$v);echo '</br>';
+            //echo "用户：".$k .'投票时间：'. date('Y-m-d H:i:s',$v);echo '</br>';
             $u_k='h:u:'.$k;
 
-            $u=Redis::hMget($u_k,['openod','nickname','sex']);
-            echo '<pre>';print_r($u);echo '</pre>'; 
+            $u=Redis::hgetAll($u_k);
+            //$u=Redis::hMget($u_k,['openid','nickname','sex','headimgurl']);
+            //echo '<pre>';print_r($u);echo '</pre>';
+            echo '<img src="'.$u['headimgurl'].'">'; 
         }
         
     }
