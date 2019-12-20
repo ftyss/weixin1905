0abb59c8944ef13edc179caf86866eeff0594f83
ftyss <1251898775@qq.com>
2019-12-20 10:29:53 +0000

    商品详情页嵌套图片

--- app/Http/Controllers/Goods/IndexController.php
+++ app/Http/Controllers/Goods/IndexController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\Goods;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Model\WxGoodsModel;
 
 class IndexController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('Goods.details');
+        $goods_id=$request->input('id');
+        $goods=WxGoodsModel::find($goods_id);
+        //echo '<pre>';print_r($goods);echo '</pre>';
+        $data=[
+            'goods'=>$goods
+        ];
+        return view('Goods.details',$data);
     }
 }
