73c1fe710cb86ab2b9203b804d420d68785404c2
ftyss <1251898775@qq.com>
2019-12-26 01:51:27 +0000

    授权

--- app/Http/Controllers/Course/CourseController.php
+++ app/Http/Controllers/Course/CourseController.php
@@ -4,8 +4,39 @@ namespace App\Http\Controllers\Course;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class CourseController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('Course.index');
+    }
+
+    /**
+     * 根据code获取access_token
+     */
+    protected function getAccessToken($code)
+    {
+        $url='https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('WX_APPID').'&secret='.env('WX_APPSECRET').'&code='.$code.'&grant_type=authorization_code';
+        $json_data=file_get_contents($url);
+        return json_decode($json_data,true);
+    }
+
+    /**
+     * 获取用户基本信息
+     */
+    protected function getUserInfo($access_token,$openid)
+    {
+        $url='https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
+        $json_data=file_get_contents($url);
+        $data=json_decode($json_data,true);
+        //echo '<pre>';print_r($usr_info);echo '</pre>';
+        if(isset($data['errcode'])){
+            die("出现错误");        //报错信息
+        }
+        return $data;       //返回用户信息
+
+    }   
+    
 }
